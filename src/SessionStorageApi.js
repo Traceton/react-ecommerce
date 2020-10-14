da70377672721106d966d624a968dec66682ee21
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-10-14 15:06:02 -0400

    completed session storage api functions saveUser/removeUser/getUser

--- src/SessionStorageApi.js
+++ src/SessionStorageApi.js
@@ -2,16 +2,46 @@
 
 // save a user to session storage
 const saveUserToSessionStorage = async (authorizedUser) => {
-  console.log("saveUserToSessionStorage");
+  const user = await authorizedUser;
+  try {
+    if (user) {
+      console.log("User saved to session storage");
+      return sessionStorage.setItem("user", user);
+    } else {
+      return console.error("No user to save in session storage");
+    }
+  } catch (error) {
+    return console.error("Could not save user in session storage");
+  }
 };
 
 // removes a user from session user
 const removeUserFromSessionStorage = async (authorizedUser) => {
-  console.log("removeUserFromSessionStorage");
+  const user = await authorizedUser;
+  try {
+    if (user) {
+      console.log("User cleared from session storage");
+      return sessionStorage.clear(user);
+    } else {
+      return console.error("Could not find user to remove in session storage");
+    }
+  } catch (error) {
+    return console.error("Could not clear session storage");
+  }
 };
 
 // checks if there is a user in session storage
 // returns user if there is.
 const getUserFromSessionStorage = async () => {
-  console.log("getUserFromSessionStorage");
+  const userFromSessionStorage = await sessionStorage.getItem("user");
+  try {
+    if (userFromSessionStorage) {
+      console.log("User Was Found in Session storage");
+      return userFromSessionStorage;
+    } else {
+      console.error("No user found in session storage");
+    }
+  } catch (error) {
+    console.error("Could not get user from session storage ");
+  }
 };
