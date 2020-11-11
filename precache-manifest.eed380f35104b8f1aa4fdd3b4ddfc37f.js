e04ef1a653fd9b77f4c33b55234729e386b4af92
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-11-11 15:20:09 -0500

    Updates

---
+++ precache-manifest.eed380f35104b8f1aa4fdd3b4ddfc37f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98635335557fcb1b881927fcea40281",
+    "url": "/react-ecommerce/react-ecommerce/index.html"
+  },
+  {
+    "revision": "a510d14eb1cd9bf5e82d",
+    "url": "/react-ecommerce/react-ecommerce/static/css/main.86709238.chunk.css"
+  },
+  {
+    "revision": "12314ef08afcfc4c0627",
+    "url": "/react-ecommerce/react-ecommerce/static/js/2.87d62c9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ecommerce/react-ecommerce/static/js/2.87d62c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a510d14eb1cd9bf5e82d",
+    "url": "/react-ecommerce/react-ecommerce/static/js/main.e539deec.chunk.js"
+  },
+  {
+    "revision": "b01fc35cc7e16bc1883b",
+    "url": "/react-ecommerce/react-ecommerce/static/js/runtime-main.6718d199.js"
+  }
+]);
