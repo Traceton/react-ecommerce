a4ebc0c9ea975263dc7af945158de052399692ac
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-11-15 23:47:39 -0500

    removed old styles component

--- src/routes/account/components/Account.js
+++ src/routes/account/components/Account.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-// import "../styles/account.css";
+
 import { UserContext } from "../../../UserContext";
 import UpdateAccount from "./UpdateAccount";
 import { API } from "../../../UserApi";
